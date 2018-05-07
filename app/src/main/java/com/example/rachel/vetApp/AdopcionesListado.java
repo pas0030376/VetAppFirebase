8e2a1bf33aeffb40fcb1970a5bec8e20c6a6bad6
Justine Zape <y3856547j@iespoblenou.org>
2018-05-07 15:40:45 +0200

    set title Adopciones

--- app/src/main/java/com/example/rachel/vetApp/AdopcionesListado.java
+++ app/src/main/java/com/example/rachel/vetApp/AdopcionesListado.java
@@ -15,12 +15,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
@@ -70,7 +68,7 @@ public class AdopcionesListado extends AppCompatActivity {
             }
         });
 
-        getAc
+        AdopcionesListado.this.setTitle("Adopciones");
 
         lvAdopciones = findViewById(R.id.lvAdopciones);
 
